459cf6d814519fafddca5f1fd1387837936e0450
graham-hughes <grahamhughes@airbears2-10-142-175-34.airbears2.1918.berkeley.edu>
2017-11-28 12:52:18 -0800

    Added web3 skeleton code

---
+++ web3.js
@@ -0,0 +1,16 @@
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider());
+
+// Set this to StoreFront address
+const contract_address;
+
+/* Purchases tokens */
+function purchasePresentokens() {
+	// var send = web3.eth.sendTransaction({from:eth.coinbase, to:contract_address, value:web3.toWei(0.05, "ether")});
+}
+
+/* Sends tokens to owner */
+function sendPresentokens() {
+
+}
